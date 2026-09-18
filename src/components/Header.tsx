79f9c3cb039cb4e80b8172699d2e8214d7512844
Hernan Robledo <rbldhernan@gmail.com>
2026-09-18 16:37:47 +0000

    integrar PanelNotificaciones en Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,18 @@
+import PanelNotificaciones from "./PanelNotificaciones";
+
 const Header = () => {
   return (
-    <header style={{ 
-      padding: "1rem 1.5rem", 
-      background: "#F2FAF0", 
+    <header style={{
+      padding: "1rem 1.5rem",
+      background: "#F2FAF0",
       borderBottom: "2px solid #A8E063",
       display: "flex",
       justifyContent: "space-between",
       alignItems: "center"
     }}>
       <h1 style={{ color: "#39A900", margin: 0, fontSize: "1.3rem" }}>ZENDA</h1>
+
+      <PanelNotificaciones />
     </header>
   );
 };
